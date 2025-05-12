b5db98f5ffb27bc0405f235efa84cb3c6cac51cb
Jaime <jaime.shae.blackwell@gmail.com>
2025-05-12 10:53:19 +1000

    Updates to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='helloworld'
+    name='GeoGals',
+    version='0.0.1',
+    description='A publicly available repository for using geostatistical methods on real and simulated galaxy data.',
+    py_modules=["GeoGals"]
 )
